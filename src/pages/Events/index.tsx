5bc4cb8711664c4816d5e21c81c694de12307426
Adam0Schulz <adam@schulz.sk>
2022-12-04 17:23:04 +0100

    deleted redundant code and added proper export/import of an eventstimeline component

--- src/pages/Events/index.tsx
+++ src/pages/Events/index.tsx
@@ -1,22 +1,10 @@
 // this import is here just so it doesn't show an error (bc there has to be an import or export present in any ts file)
-import { get } from "api/calls/Events";
-import { Event } from "api/models";
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
-import EventsTimeline from "features/Event/components/EventsTimeline";
-import { useEffect, useState } from "react";
+import { EventsTimeline } from "features/Event";
 import './style.css'
 
 const Events = () => {
-    const [events, setEvents] = useState<Event[] | null>(null)
-    let row = 0
-    useEffect(() => {
-        async function getThingy() {
-            setEvents(await get());
-        }
-        getThingy()
-
-    }, [])
     return (
         <>
         <div className="page-cont">
