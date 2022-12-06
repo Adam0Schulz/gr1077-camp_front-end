8ce05299d0e781ccc8ff1ec74ca386fabca62851
Adam0Schulz <adam@schulz.sk>
2022-12-06 10:25:50 +0100

    fixed import path in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 // this import is here just so it doesn't show an error (bc there has to be an import or export present in any ts file)
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
-import Slider from "../../features/Event/components/EventSlider/EventSlider";
+import Slider from "features/Event/components/EventSlider/EventSlider";
 
 const Home = () => {
     return (
