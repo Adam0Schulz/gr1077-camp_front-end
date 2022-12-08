ef214237afeef8e637105605635005f52f9fd6d8
brrobert17 <73032043+brrobert17@users.noreply.github.com>
2022-12-08 14:39:38 +0100

    regEvent page in progress5

--- src/pages/RegisterToEvent/RegisterToEventPage.tsx
+++ src/pages/RegisterToEvent/RegisterToEventPage.tsx
@@ -3,23 +3,30 @@ import 'App.css'
 import './RegisterToEventPage.css'
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
-import {ScrollDownArrow} from "../../components/ScrollDownArrow/ScrollDownArrow";
 import PostHeader from "../../features/Event/components/PostHeader/PostHeader";
 import {useParams} from "react-router-dom";
 import {useEventById} from "../../features/Event/hooks/UseAllEvents";
 import {EventDescription} from "../../features/Event/components/EventDescription/EventDescription";
+import {EventSideBar} from "../../features/Event/components/EventSideBar/EventSideBar";
 
 
 export const RegisterToEventPage = () => {
     const eventId = Number(useParams().id);
-    const { data: event, isLoading, isError, error, isFetching } = useEventById(eventId as number);
+    const {data: event, isLoading, isError, error, isFetching} = useEventById(eventId as number);
 
     return (
-        <div>
+        <div className={"register-to-event-page"}>
             <div className={"page-cont"}>
                 <Navbar activePage={'events'}/>
-                {event && <PostHeader page={event}/>}
-                {event && <EventDescription event={event}/>}
+                <div className={"register-to-event-grid"}>
+                    <div>
+                        {event && <PostHeader page={event}/>}
+                        {event && <EventDescription event={event}/>}
+                    </div>
+                    <div style={{marginTop: "3.2 em"}}>
+                        <EventSideBar/>
+                    </div>
+                </div>
                 <RegisterToEventForm/>
             </div>
             <Footer/>
